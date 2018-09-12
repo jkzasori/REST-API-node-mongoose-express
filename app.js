d229a8c19efc15a32729a9fc79d4574aeec40c0e
José Támara <jochitoel1@hotmail.com>
2018-09-12 12:47:38 -0500

    Passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -44,27 +46,24 @@ app.use(session({
 	store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', index);
 app.use('/users', users);
 
 function auth (req, res, next) {
-	console.log(req.session);
+	console.log(req.user);
 
-	if (!req.session.user) {
+	if (!req.user) {
 		var err = new Error('You are not authenticated!');
+		res.setHeader('WWW-Authenticate', 'BAsic');
 		err.status = 403;
 		return next(err);
 		
 	}
 	else {
-		if (req.session.user === 'authenticated') {
-			next();
-		}
-		else {
-			var err = new Error('You are not authenticated!');
-			err.status = 403;
-			return next(err);
-		}
+		next();
 	}	
 }
 
